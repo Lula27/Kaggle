ff538584f61f313e16814e843d37aa93d5bc6aac
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-12-07 21:44:58 -0500

    added r analysis file

---
+++ RAnalysis.R
@@ -0,0 +1,12 @@
+/* Titanic Data Set Analysis */ 
+  
+/* Open datasets */ 
+  
+survival <- read.csv('C:\\Users\\loret\\Desktop\\DataSciencePrep\\Kaggle\\Titanic\\survival.csv', header = TRUE)
+test <- read.csv('C:\\Users\\loret\\Desktop\\DataSciencePrep\\Kaggle\\Titanic\\test.csv', header = TRUE)
+train <- read.csv('C:\\Users\\loret\\Desktop\\DataSciencePrep\\Kaggle\\Titanic\\train.csv', header = TRUE)
+
+View(survival)
+str(survival)
+str(test)
+str(train)
