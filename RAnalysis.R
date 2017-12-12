41a99d42a6f8b2935224a375897eb9282f2af10b
Loretta Agyemang <loretta.agyemang@gmail.com>
2017-12-11 19:25:53 -0500

    have a target of what submission looks like

--- RAnalysis.R
+++ RAnalysis.R
@@ -1,12 +1,16 @@
-/* Titanic Data Set Analysis */ 
+# Titanic Data Set Analysis 
   
-/* Open datasets */ 
+# Open datasets 
   
 survival <- read.csv('C:\\Users\\loret\\Desktop\\DataSciencePrep\\Kaggle\\Titanic\\survival.csv', header = TRUE)
 test <- read.csv('C:\\Users\\loret\\Desktop\\DataSciencePrep\\Kaggle\\Titanic\\test.csv', header = TRUE)
 train <- read.csv('C:\\Users\\loret\\Desktop\\DataSciencePrep\\Kaggle\\Titanic\\train.csv', header = TRUE)
+final <- read.csv('C:\\Users\\loret\\Desktop\\DataSciencePrep\\Kaggle\\Titanic\\gender_submission.csv', header = TRUE)
 
-View(survival)
-str(survival)
-str(test)
-str(train)
+# Gender_submission = survival
+# Essentially, this is a prediction of someone surviving based on them being a female. (survival | female)
+
+
+View(final)
+str(final)
+
