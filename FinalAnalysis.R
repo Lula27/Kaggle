636cf1addc7bccdb49c5616b7228d54c78a288a0
Loretta Agyemang <loretta.agyemang@gmail.com>
2018-02-04 17:12:36 -0500

    loaded datasets

--- FinalAnalysis.R
+++ FinalAnalysis.R
@@ -1,30 +1,42 @@
 # Open the CSV file exported through Jupytr Notebook
 # Export files to csv 
 browseURL("http://www.instantr.com/2012/12/11/exporting-a-dataset-from-r/")
-
-# Read datasets 
-myData = read.csv("C:\\Users\\loret\\Desktop\\DataSciencePrep\\Kaggle\\submit.csv", header = TRUE)
-myData
-
-# submission should be in CSV format with 418 prediction rows + header row 
-
-# Get structure 
-str(myData) # there are 891 obs and 10 variables - reduce variables to 2 (ID & survived)
-dataTwo = myData[ ,2:3]
-View(dataTwo) 
-rm(dataTwo)
-
-Tsurvival = dataTwo[1:418, ]
-View(Tsurvival)
-rm(Fdata)
+browseURL("https://github.com/anurajaram/kaggle-titanic/blob/master/tree.R")
 
 
-# Export final dataset as csv
-write.csv(Tsurvival, "C:\\Users\\loret\\Desktop\\DataSciencePrep\\Kaggle\\laTitanic.csv")
-
-# Don't forget to remove extra added column on the far left
+# Don't forget to remove extra added column on the far left - for exporting dataset
 write.csv(Tsurvival, "C:\\Users\\loret\\Desktop\\DataSciencePrep\\Kaggle\\LA_Titanic.csv", row.names=FALSE)
 
 # Clear workspace
 rm(list = ls())
 
+
+# Use Decision Trees to Analyse Datasets 
+
+# Access libraries 
+library(tree)
+library(ISLR)
+library(rpart)
+library(MASS)
+
+# y_train: dependent variable 
+# x_train: independent variable 
+# x : training data 
+
+
+?cbind
+
+
+# Try with Anu Rajaram's work
+# Load and label dataframes 
+# A: train dataset 
+?read.delim # Reads a file in table format and creates a data frame from it, with cases corresponding to lines and variables to fields in the file.
+train = read.delim(file = 'C:\\Users\\loret\\Desktop\\DataSciencePrep\\Kaggle\\Titanic\\cleantrain.csv', header = TRUE,
+                    sep = ',', dec = '.')
+
+View(train)
+
+# B: test dataset 
+test = read.delim(file = 'C:\\Users\\loret\\Desktop\\DataSciencePrep\\Kaggle\\Titanic\\cltest.csv', header = TRUE,
+                    sep = ',', dec = '.')
+View(test)
